22c24e4c8f316238aceda6c9cbb16ca38ffb3d6b
yahya.yusuf <webyahyaver@gmail.com>
2025-12-05 07:47:13 +0400

    externalize to json and use arena

--- dotfiler.zig
+++ dotfiler.zig
@@ -1,11 +1,11 @@
 const std = @import("std");
-const ArrayList = std.ArrayList;
+const io = std.io;
 const Mapping = struct {
     source: []const u8,
     target: []const u8,
 };
 const Config = struct {
-    mappings: *ArrayList(Mapping),
+    mappings: []Mapping,
 };
 
 pub fn main() !void {
@@ -17,19 +17,31 @@ pub fn main() !void {
     }
 
     const allocator = gpa.allocator();
-    var mappings = try ArrayList(Mapping).initCapacity(allocator, 1);
-    defer mappings.deinit(allocator);
+    var arenaAllocator = std.heap.ArenaAllocator.init(allocator);
+    defer arenaAllocator.deinit();
+    const arena = arenaAllocator.allocator();
 
-    const mapping = Mapping{
-        .source = "~/.config/ghostty/config",
-        .target = "~/dotfiles/ghostty/config",
-    };
+    const file = try std.fs.cwd().openFile("map.json", .{ .mode = .read_only });
+    defer file.close();
 
-    try mappings.append(allocator, mapping);
+    const slice = try file.readToEndAlloc(arena, 1024 * 1024); // read a max of 1MB for now
+    const json = try std.json.parseFromSliceLeaky([]Mapping, arena, slice, .{ .allocate = .alloc_always });
 
     const config = Config{
-        .mappings = &mappings,
+        .mappings = json,
     };
 
-    std.debug.print("First mapping: {s} -> {s}\n", .{ config.mappings.items[0].source, config.mappings.items[0].target });
+    const resolved_source = try resolvePath(arena, config.mappings[0].source);
+    const resolved_target = try resolvePath(arena, config.mappings[0].target);
+    std.debug.print("First mapping: {s} -> {s}\n", .{ config.mappings[0].source, config.mappings[0].target });
+    std.debug.print("Resolved path: {s} -> {s}\n", .{ resolved_source, resolved_target });
+}
+
+pub fn resolvePath(arena: std.mem.Allocator, path: []const u8) ![]const u8 {
+    const hasHome = std.mem.startsWith(u8, path, "~");
+    if (hasHome) {
+        const homeDir = try std.process.getEnvVarOwned(arena, "HOME");
+        return std.fs.path.join(arena, &[_][]const u8{ homeDir, path[1..] });
+    }
+    return path;
 }
