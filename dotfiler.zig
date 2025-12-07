8708c0be6993d600aac000eec03b29d7de7a603f
Brandtall <webyahyaver@gmail.com>
2025-12-07 14:17:09 +0400

    add dry run and flip source, target

--- dotfiler.zig
+++ dotfiler.zig
@@ -21,6 +21,22 @@ pub fn main() !void {
     defer arenaAllocator.deinit();
     const arena = arenaAllocator.allocator();
 
+    const homeDir = try std.process.getEnvVarOwned(arena, "HOME");
+
+    var argsWithPath = try std.process.argsWithAllocator(arena);
+    defer argsWithPath.deinit();
+    var dryRun: bool = false;
+
+    if (!argsWithPath.skip()) {
+        return;
+    }
+    while (argsWithPath.next()) |arg| {
+        if (std.mem.eql(u8, arg, "--dry-run")) {
+            std.debug.print("Plan mode\n", .{});
+            dryRun = true;
+        }
+    }
+
     const file = try std.fs.cwd().openFile("map.json", .{ .mode = .read_only });
     defer file.close();
 
@@ -32,9 +48,8 @@ pub fn main() !void {
     };
 
     for (config.mappings) |path| {
-
-        const resolved_source = try resolvePath(arena, path.source);
-        const resolved_target = try resolvePath(arena, path.target);
+        const resolved_source = try resolvePathWithHome(arena, path.source, homeDir);
+        const resolved_target = try resolvePathWithHome(arena, path.target, homeDir);
 
         std.fs.accessAbsolute(resolved_source, .{}) catch {
             std.debug.print("Source not found\n", .{});
@@ -44,23 +59,27 @@ pub fn main() !void {
         const parent_dir_path = std.fs.path.dirname(resolved_target) orelse
             return error.InvalidPath;
 
-        try std.fs.cwd().makePath(parent_dir_path); 
-        
-        std.fs.symLinkAbsolute(resolved_source, resolved_target, .{}) catch |err| {
-            if (err == error.PathAlreadyExists) {
-                std.debug.print("Skipping: Target already exists\n", .{});
-                continue;
-            }
-            return err;
-    };
-        std.debug.print("Linked\n", .{});
+        if (dryRun) {
+            std.debug.print("Create a directory: {s} \n", .{parent_dir_path});
+            std.debug.print("Create a symlink from: {s} to: {s} \n", .{resolved_source, resolved_target});
+
+        } else {
+            try std.fs.cwd().makePath(parent_dir_path);
+            std.fs.symLinkAbsolute(resolved_source, resolved_target, .{}) catch |err| {
+                if (err == error.PathAlreadyExists) {
+                    std.debug.print("Skipping: Target already exists\n", .{});
+                    continue;
+                }
+                return err;
+            };
+            std.debug.print("Linked\n", .{});
+        }
     }
 }
 
-pub fn resolvePath(arena: std.mem.Allocator, path: []const u8) ![]const u8 {
+pub fn resolvePathWithHome(arena: std.mem.Allocator, path: []const u8, homeDir: []const u8) ![]const u8 {
     const hasHome = std.mem.startsWith(u8, path, "~");
     if (hasHome) {
-        const homeDir = try std.process.getEnvVarOwned(arena, "HOME");
         return std.fs.path.join(arena, &[_][]const u8{ homeDir, path[1..] });
     }
     return path;
